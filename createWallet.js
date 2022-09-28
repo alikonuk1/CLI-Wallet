5ac6afa42a5fd1b4ea2e5a7467577dd8f7e69ac5
ali konuk <alikonuk1@gmail.com>
2022-09-28 23:23:32 +0300

    spacing

--- createWallet.js
+++ createWallet.js
@@ -2,9 +2,15 @@ const ethers = require('ethers');
 
 const createWallet = async () => {
     const wallet = ethers.Wallet.createRandom();
+    console.log(` `);
+    console.log(` `);
     console.log(`Private Key = ${wallet.privateKey}`);
+    console.log(` `);
     console.log(`Mnemonic = ${wallet.mnemonic.phrase}`);
+    console.log(` `);
     console.log(`Public Address = ${wallet.address}`);
+    console.log(` `);
+    console.log(` `);
 }
 
 createWallet();
